9e182266910fa51532d79ef3bd2068a987fa03f4
shinhonggyu <zowoz0@naver.com>
2022-03-23 16:21:52 +0900

    복습

--- api/index.js
+++ api/index.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { videos, user } = require('./fakeData');
+const { videos, user } = require("./fakeData");
 app.use(express.json());
 
-app.get('/api/user/1', (req, res) => {
+app.get("/api/user/1", (req, res) => {
   setTimeout(() => {
     res.json(user);
   }, 3000);
 });
 
-app.get('/api/videos/1', (req, res) => {
+app.get("/api/videos/1", (req, res) => {
   setTimeout(() => {
     res.json(videos);
-  }, 5000);
+  }, 3000);
 });
 
 app.listen(8800, () => {
-  console.log('Backend server is running!');
+  console.log("Backend server is running!");
 });
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,7 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import './app.css';
-import Feed from './components/Feed/Feed';
-import Sidebar from './components/Sidebar/Sidebar';
-import Topbar from './components/Topbar/Topbar';
+import "./app.css";
 
-function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [user, setUser] = useState({});
-
-  useEffect(() => {
-    const getUser = async () => {
-      setIsLoading(true);
-      try {
-        const res = await axios.get('/api/user/1');
-        setUser(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-      setIsLoading(false);
-    };
-    getUser();
-  }, []);
-  return (
-    <div className="container">
-      <Sidebar isLoading={isLoading} />
-      <div className="home">
-        <Topbar isLoading={isLoading} user={user} />
-        <Feed />
-      </div>
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
